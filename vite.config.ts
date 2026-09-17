935a78d46fca5a6971f3b0a702a79f06821fd84e
yamanami1207-alt <yamanami1207@gmail.com>
2026-09-17 14:03:27 +0900

    new!

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        stonework: resolve(__dirname, 'stonework/index.html'),
+      },
+    },
+  },
+});
